1246389bacec4d3fa4029f43ef94a9f46d60fa66
Abdulrahman Alkhalidi <abdulrahmankhalidi003@gmail.com>
2026-05-28 07:44:02 +0300

    [US-Auth] Add user registration, login and logout

--- app.py
+++ app.py
@@ -1,8 +1,76 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import sqlite3
+from database import init_db, get_db
+from werkzeug.security import  generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
-app.secret_key = 'your-secret-key-change-this'
+app.secret_key = 'abdu1234'
+
+init_db()
+
+
+#auth_routes
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed = generate_password_hash(password)
+
+        db = get_db()
+        try:
+            db.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, hashed))
+            db.commit()
+            return redirect(url_for('login'))
+        except:
+            flash('Username already taken.')
+            return redirect(url_for('register'))
+        finally:
+            db.close()
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        db = get_db()
+        user = db.execute('SELECT * FROM users WHERE username = ?', (username,)).fetchone()
+        db.close()
+
+        if user and check_password_hash(user['password'], password):
+            session['user_id'] = user['id']
+            session['username'] = user['username']
+            return redirect(url_for('inventory'))
+        else:
+            flash('Invalid username or password.')
+            return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+
+#placeHolder - crash prevention
+@app.route('/inventory')
+def inventory():
+    return 'Inventory page coming soon'
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -1,6 +1,14 @@
 import sqlite3
 
+def init_db():
+    conn = get_db()
+    with open('schema.sql', 'r') as f:
+        conn.executescript(f.read())
+    conn.commit()
+    conn.close()
+
 def get_db():
-    conn = sqlite3.connect('shop.db')
+    conn = sqlite3.connect('mechanic.db')
     conn.row_factory = sqlite3.Row  # lets you access columns by name like row['name']
     return conn
+
